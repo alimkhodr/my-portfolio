8f4dd8d899a6bc0e549f00e1c9bf70111144b7c7
Ali Mohamed <ali.m.khodr@gmail.com>
2024-07-24 23:49:03 -0300

    feat: add OnClik at Styled

--- src/components/StyledButton/StyledButton.tsx
+++ src/components/StyledButton/StyledButton.tsx
@@ -1,28 +1,29 @@
 import { styled } from "@mui/material";
-import { ReactNode } from "react";
+import { ReactNode, MouseEventHandler } from "react";
 
 interface StyledButtonProps {
   children: ReactNode;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
-const StyledButton: React.FC<StyledButtonProps> = ({ children }) => {
-    return <CustomButton>{children}</CustomButton>;
-  };  
+const CustomButton = styled("button")(({ theme }) => ({
+  backgroundColor: "transparent",
+  border: `1px solid ${theme.palette.primary.contrastText}`,
+  borderRadius: "3px",
+  padding: "5px 15px",
+  width: "100%",
+  display: "inline-flex",
+  alignItems: "center",
+  justifyContent: "center",
+  gap: "10px",
+  color: theme.palette.primary.contrastText,
+  '&:hover': {
+    backgroundColor: theme.palette.secondary.main,
+  },
+}));
 
-  const CustomButton = styled("button")(({ theme }) => ({
-    backgroundColor: "transparent",
-    border: `1px solid ${theme.palette.primary.contrastText}`,
-    borderRadius: "3px",
-    padding: "5px 15px",
-    width: "100%",
-    display: "inline-flex",
-    alignItems: "center",
-    justifyContent: "center",
-    gap: "10px",
-    color: theme.palette.primary.contrastText,
-    '&:hover': {
-      backgroundColor: theme.palette.secondary.main,
-    },
-  }));
-  
-  export default StyledButton;
+const StyledButton: React.FC<StyledButtonProps> = ({ children, onClick }) => {
+  return <CustomButton onClick={onClick}>{children}</CustomButton>;
+};
+
+export default StyledButton;
