0bb2fd1eeb2322cb127da3787f2cf1e5910b1122
Ali Mohamed <ali.m.khodr@gmail.com>
2024-07-26 00:17:19 -0300

    style: change the primary color

--- src/theme.ts
+++ src/theme.ts
@@ -3,7 +3,7 @@ import { createTheme, responsiveFontSizes } from "@mui/material";
 let theme = createTheme({
     palette: {
         primary: {
-            main: '#101010',
+            main: '#0a0a0a',
         },
         secondary: {
             main: '#0057FF',
