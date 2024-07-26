04d9c88a22d24b3cd1d0698c27dbe45800a65d96
Ali Mohamed <ali.m.khodr@gmail.com>
2024-07-26 00:19:02 -0300

    feat: creates the section about

---
+++ src/pages/Home/sections/About/About.tsx
@@ -0,0 +1,96 @@
+import { Container, Grid, styled, Typography } from "@mui/material";
+import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
+import SchoolIcon from '@mui/icons-material/School';
+import type { SVGProps } from 'react';
+
+export function FluentMdl2ReactLogo(props: SVGProps<SVGSVGElement>) {
+    return (<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 2048 2048" {...props}><path fill="currentColor" d="M2048 1039q0 47-21 88t-57 75t-81 63t-92 51t-94 40t-82 28q15 63 24 126t9 127q0 53-9 109t-34 101t-67 75t-108 29q-55 0-112-21t-111-55t-102-72t-87-75q-38 36-86 75t-103 72t-111 54t-112 22q-65 0-107-29t-67-74t-34-102t-10-109q0-64 9-127t24-126q-37-11-82-28t-93-39t-93-51t-80-63t-57-76t-22-88q0-47 21-88t57-75t81-63t92-51t94-40t82-28q-15-63-24-126t-9-127q0-53 9-109t34-101t67-75t108-29q55 0 112 21t111 55t102 72t87 76q38-36 86-75t103-73t111-54t112-22q65 0 107 29t67 74t34 102t10 109q0 64-9 127t-24 126q36 11 82 28t93 39t93 51t80 63t57 76t22 88m-612-816q-43 0-91 20t-94 49t-88 64t-71 62q47 49 88 101t81 107q69 6 136 17t133 27q13-56 21-113t8-114q0-30-4-68t-16-72t-37-57t-66-23m-834 930q-17 41-31 81t-28 83q43 11 86 18t87 13q-31-48-59-96t-55-99m-59-392q13 42 27 83t32 82q26-51 55-99t59-97q-44 6-87 13t-86 18m109 278q80 166 186 322q47 4 93 5t93 2q47 0 93-1t93-6q106-156 186-322q-42-85-87-164t-99-158q-47-3-93-5t-93-2t-93 2t-93 5q-53 78-98 157t-88 165m680 309q44-5 87-12t86-19q-13-42-27-82t-32-82q-26 51-54 99t-60 96m114-422q17-41 31-82t28-83q-43-10-86-17t-87-14q30 48 59 96t55 100m-311-309q-26-35-53-67t-58-65q-30 32-57 64t-54 68q55-3 111-3q55 0 111 3M489 443q0 57 8 114t21 113q66-16 133-27t136-17q39-55 80-107t89-101q-30-28-71-62t-87-63t-95-50t-91-20q-41 0-65 23t-37 56t-17 72t-4 69m-38 849q20-65 44-128t52-125q-28-62-52-125t-44-128q-24 7-59 19t-74 30t-79 40t-71 48t-52 55t-20 61q0 32 19 61t51 55t71 48t79 39t75 30t60 20m161 563q43 0 91-20t94-49t88-63t71-62q-47-49-88-101t-81-108q-68-6-135-17t-134-26q-13 56-21 112t-8 114q0 31 3 69t17 72t37 56t66 23m301-393q52 69 111 132q58-63 111-132q-56 3-111 3q-57 0-111-3m646 173q0-57-8-113t-21-113q-67 15-134 26t-135 17q-39 55-80 107t-89 102q30 28 71 61t87 63t95 50t91 20q41 0 65-22t37-56t17-72t4-70m38-343q24-7 59-19t75-30t79-40t71-48t51-55t20-61q0-32-20-61t-51-55t-71-48t-79-39t-75-30t-59-20q-20 65-44 128t-52 125q28 62 52 125t44 128m-573-62q-39 0-74-15t-60-41t-41-61t-15-74q0-39 15-74t40-60t61-41t74-15q39 0 74 15t60 40t41 61t15 74q0 39-15 74t-40 61t-61 41t-74 15"></path></svg>);
+}
+
+export function DeviconPlainCsharp(props: SVGProps<SVGSVGElement>) {
+    return (<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 128 128" {...props}><path fill="currentColor" d="m117.5 33.5l.3-.2c-.6-1.1-1.5-2.1-2.4-2.6L67.1 2.9c-.8-.5-1.9-.7-3.1-.7s-2.3.3-3.1.7l-48 27.9c-1.7 1-2.9 3.5-2.9 5.4v55.7c0 1.1.2 2.3.9 3.4l-.2.1c.5.8 1.2 1.5 1.9 1.9l48.2 27.9c.8.5 1.9.7 3.1.7s2.3-.3 3.1-.7l48-27.9c1.7-1 2.9-3.5 2.9-5.4V36.1c.1-.8 0-1.7-.4-2.6m-53.5 70c-21.8 0-39.5-17.7-39.5-39.5S42.2 24.5 64 24.5c14.7 0 27.5 8.1 34.3 20l-13 7.5C81.1 44.5 73.1 39.5 64 39.5c-13.5 0-24.5 11-24.5 24.5s11 24.5 24.5 24.5c9.1 0 17.1-5 21.3-12.4l12.9 7.6c-6.8 11.8-19.6 19.8-34.2 19.8M115 62h-3.2l-.9 4h4.1v5h-5l-1.2 6h-4.9l1.2-6h-3.8l-1.2 6h-4.8l1.2-6H94v-5h3.5l.9-4H94v-5h5.3l1.2-6h4.9l-1.2 6h3.8l1.2-6h4.8l-1.2 6h2.2zm-12.7 4h3.8l.9-4h-3.8z"></path></svg>);
+}
+
+const About = () => {
+    const StyledAbout = styled("div")(({ theme }) => ({
+        backgroundColor: theme.palette.primary.contrastText,
+        display: "flex",
+        alignItems: "center",
+        paddingTop: "50px",
+        paddingBottom: "50px"
+    }));
+
+    const StyledCard = styled("div")(() => ({
+        borderRadius: "10px",
+        border: "1px solid",
+        width: "100%",
+        padding: "10px 0",
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        justifyContent: "center"
+    }));
+
+    const StyledTech = styled("div")(() => ({
+        borderRadius: "10px",
+        border: "1px solid",
+        padding: "10px",
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "row",
+        justifyContent: "center",
+        gap: "5px"
+    }));
+
+    const StyledHr = styled("hr")(({ theme }) => ({
+        border: "none",
+        borderTop: `1px solid ${theme.palette.primary.main}`,
+        width: "100%",
+        margin: "30px auto"
+    }));
+
+    return (
+        <>
+            <StyledAbout>
+                <Container maxWidth="lg">
+                    <Typography color="primary.main" variant="h2" textAlign="center">About me</Typography>
+                    <Grid container display="flex" justifyContent="center" spacing={2} pt={4} pb={4}>
+                        <Grid item xs={8} md={3} display="flex" justifyContent="center">
+                            <StyledCard>
+                                <WorkspacePremiumIcon />
+                                <Typography color="primary.main" textAlign="center" fontWeight={"bold"}>Experience</Typography>
+                                <Typography color="primary.main" textAlign="center">+1 year at APTIV</Typography>
+                            </StyledCard>
+                        </Grid>
+                        <Grid item xs={8} md={3} display="flex" justifyContent="center">
+                            <StyledCard>
+                                <SchoolIcon />
+                                <Typography color="primary.main" textAlign="center" fontWeight={"bold"}>Education</Typography>
+                                <Typography color="primary.main" textAlign="center">4/6 at FATEC...</Typography>
+                                <Typography color="primary.main" textAlign="center">5/10 at UNIVAP...</Typography>
+                            </StyledCard>
+                        </Grid>
+                    </Grid>
+                    <Typography color="primary.main" textAlign="justify">
+                        Profissional de Tecnologia motivado e dedicado, com formação técnica em Desenvolvimento de Sistemas e foco atual em Banco de Dados e Análise de Sistemas. Avançado em inglês, possuo habilidades avançadas em metodologias ágeis, como Scrum, e em princípios de melhoria contínua, como Kaizen.
+                        Mantenho-me atualizado constantemente, buscando a excelência técnica e alinhamento com as últimas tendências do setor.
+                        Com uma abordagem organizada e meticulosa, tanto no ambiente físico quanto na programação, busco contribuir para ambientes dinâmicos com minha paixão pela tecnologia e comprometimento com a eficiência e a inovação.
+                    </Typography>
+                    <StyledHr />
+                    <Typography color="primary.main" variant="h2" textAlign="center">Skills</Typography>
+                    <Grid container display="flex" justifyContent="center" spacing={2} pt={4} pb={4} gap={2}>
+                        <StyledTech>
+                            <DeviconPlainCsharp />
+                            <Typography color="primary.main" textAlign="center" fontWeight={"bold"}>C#</Typography>
+                        </StyledTech>
+                        <StyledTech>
+                            <FluentMdl2ReactLogo />
+                            <Typography color="primary.main" textAlign="center" fontWeight={"bold"}>React</Typography>
+                        </StyledTech>
+                    </Grid>
+                </Container>
+            </StyledAbout>
+        </>
+    )
+}
+export default About
